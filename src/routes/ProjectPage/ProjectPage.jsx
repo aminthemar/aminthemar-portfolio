e02cae1181590cfd88115de3354f8e39e861bfdc
unknown <aminthemar@hotmail.com>
2023-03-01 02:54:12 +0330

    SEO_1

--- src/routes/ProjectPage/ProjectPage.jsx
+++ src/routes/ProjectPage/ProjectPage.jsx
@@ -2,12 +2,19 @@ import { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { Project } from '../../containers';
 import { Navbar } from '../../components';
+import { works_data } from '../../constants';
 
 const ProjectPage = ({ title }) => {
     const { id } = useParams();
 
     useEffect(() => {
-        document.title = `${id.toUpperCase()} | ${title}`;
+        works_data.getWorkbyId(id)
+            .then(data => {
+                document.title = `${id.toUpperCase()[0]}${id.slice(1)} - ${data.content["title"]} | ${title}`;
+            })
+            .catch(err => {
+                console.error(err);
+            });
     }, [title, id]);
 
     return (
