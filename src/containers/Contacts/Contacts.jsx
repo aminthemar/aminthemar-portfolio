1dd753f94253482f580488e9bffa5272dbbb091c
aminthemar <aminthemar@hotmail.com>
2025-10-23 19:30:17 +0330

    edu edit

--- src/containers/Contacts/Contacts.jsx
+++ src/containers/Contacts/Contacts.jsx
@@ -105,9 +105,7 @@ const Contacts = () => {
             <div className="app__flex app__contacts-form">
               <span>
                 <h2 className="p-h2">راه‌های ارتباطی</h2>
-                <p className="p-text">
-                  می‌توانید درخواست همکاری خود را از طریق این فرم یا شبکه‌های اجتماعی زیر ارسال کنید. پیام شما تا حداکثر دو روز بررسی خواهد شد.
-                </p>
+                <p className="p-text">می‌توانید درخواست همکاری خود را از طریق این فرم یا شبکه‌های اجتماعی زیر ارسال کنید.</p>
                 <ul className="h-card app__flex-start app__contacts-social">
                   <span className="p-name footer-name visually-hidden">Mohammad Amin Rashid</span>
                   <span className="p-name footer-name visually-hidden">محمدامین رشید</span>
@@ -171,7 +169,7 @@ const Contacts = () => {
                   title="project-trailer"
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d12954.687022097798!2d51.35864307701019!3d35.73429043689173!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e073b0e459749%3A0x31816dd54a095378!2sGisha%2C%20District%202%2C%20Tehran%2C%20Tehran%20Province%2C%20Iran!5e0!3m2!1sen!2sfr!4v1660638091029!5m2!1sen!2sfr"
                   width="100%"
-                  height="198"
+                  height="222"
                   loading="lazy"
                   style={{ border: "0", borderRadius: "12px" }}
                 ></iframe>
